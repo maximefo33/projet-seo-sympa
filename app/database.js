1a4f90f4cf5d3ca63b3a427f47fc38ce1581b8d8
Elise-r01 <elise01rou@gmail.com>
2025-06-18 17:02:28 +0200

    database Relation finished - tested with psql - script create_db_relation OK

--- app/database.js
+++ app/database.js
@@ -1,19 +1,19 @@
-// ici la table relation liée à la database seo_sympa
+// ici la database seo_sympa
 
-// see @ https://github.com/O-clock-Behemoth/Fiches-Objectifs/blob/main/saison6/e14.md - utilisation de sequelize pour communiquer avec notre base de données
+// see @ https://github.com/O-clock-Behemoth/Fiches-Objectifs/blob/main/saison6/e14.md => utilisation de sequelize pour communiquer avec notre base de données
 
 // 1 npm install sequelize pg 
-// 2 config
+// 2 config avec ce code ci-dessous :
 
 import Sequelize from 'sequelize';
+import "dotenv/config"; 
 
-const sequelize = new Sequelize(process.env.PG_URL, {
-    define: {
-        underscored : true, // pour utiliser le snake_case côté SQL
-    }
-});
+// ??? en a-t-on besoin ? si oui : npm install dotenv --save
 
+const sequelize = new Sequelize(process.env.PG_URL);
 
+// PENSER A mettre notre URL postgreSQL dans .env
+// PG_URL=postgres://user:pass@example.com:5432/seo_sympa en changeant USER
 
 
 export default sequelize;
